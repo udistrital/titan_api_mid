7d3d43bd26594c4dc160e09063bda9cafedda796
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-09-28 09:08:03 -0500

    Agregada función para registro de detalle_novedad en seguridad social
    * Se agrego servicio para registrar el detalle de la novedad en seguridad social
    
    * Se agrego el servicio para eliminar cada uno de los registros de conceptos en titan

--- controllers/concepto_nomina_por_persona.go
+++ controllers/concepto_nomina_por_persona.go
@@ -27,19 +27,41 @@ func (c *Concepto_nomina_por_personaController) URLMapping() {
 func (c *Concepto_nomina_por_personaController) TrRegistroIncapacidades() {
 	var (
 		// incapacidades                                    TrConceptosNomPersona // parámetro
-		incapacidades                                               map[string][]map[string]interface{} // parámetro
-		titanCrudResponse/*, ssCrudReponse*/ map[string]interface{}                                     // respuesta del api de titan y seguridad social respectivamente
+		incapacidades                    map[string][]map[string]interface{} // parámetro
+		titanCrudResponse, ssCrudReponse map[string]interface{}              // respuesta del api de titan y seguridad social respectivamente
 	)
 	try.This(func() {
 		json.Unmarshal(c.Ctx.Input.RequestBody, &incapacidades)
 		err := sendJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+
 			"/"+beego.AppConfig.String("Nscrud")+"/concepto_nomina_por_persona/TrConceptosPorPersona", "POST", &titanCrudResponse, &incapacidades)
 		if err != nil {
-			panic(err.Error())
+			beego.Error()
+			panic(titanCrudResponse)
 		}
-		// err = sendJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+
-		// 	"/"+beego.AppConfig.String("Nscrud")+"/concepto_nomina_por_persona/TrConceptosPorPersona", "POST", &titanCrudResponse, &incapacidades)
-		c.Data["json"] = titanCrudResponse
+
+		idNovedades := titanCrudResponse["Body"].([]interface{})
+		beego.Info("idNovedades: ", idNovedades)
+		for i, id := range idNovedades {
+			incapacidades["Conceptos"][i]["Id"] = int(id.(float64))
+		}
+		infoNovedades := incapacidades["Conceptos"]
+		beego.Info(infoNovedades)
+
+		err = sendJson("http://"+beego.AppConfig.String("UrlSScrud")+":"+beego.AppConfig.String("PortSS")+
+			"/"+beego.AppConfig.String("NSSS")+"/detalle_novedad_seguridad_social/tr_registrar_detalle", "POST", &ssCrudReponse, &infoNovedades)
+		if err != nil {
+			beego.Error("ss_crud_api error")
+			for _, id := range idNovedades {
+				err := sendJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+
+					"/"+beego.AppConfig.String("Nscrud")+"/concepto_nomina_por_persona", "DELETE", &titanCrudResponse, &id)
+				if err != nil {
+					panic(titanCrudResponse)
+				}
+			}
+			panic(ssCrudReponse)
+		}
+
+		c.Data["json"] = ssCrudReponse
 
 	}).Catch(func(e try.E) {
 		beego.Error("error en TrRegistroIncapacidades: ", e)
