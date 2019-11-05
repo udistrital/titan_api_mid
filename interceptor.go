7bb11c19c4b041101d634f15f47a3d4fae159a4c
Oscar Duarte <o.duarte7@gmail.com>
2019-11-05 11:03:27 -0500

    Se realiza ajuste en el archivo interceptor para el manejo de la auditoria

--- interceptor.go
+++ interceptor.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-  "github.com/astaxie/beego"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
-  "github.com/udistrital/utils_oas/request"
+	"github.com/udistrital/utils_oas/request"
 )
 
 func InterceptMidRequest(ctx *context.Context) {
-  request.SetHeader(ctx)
-
+	request.SetHeader(ctx.Request.Header["Authorization"][0])
 }
 
 func InitInterceptor() {
-	 beego.InsertFilter("*", beego.BeforeExec, InterceptMidRequest, false)
+	beego.InsertFilter("*", beego.BeforeExec, InterceptMidRequest, false)
 }
