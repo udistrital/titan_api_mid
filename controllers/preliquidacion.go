55cd2f415e32552c9014bcdc9f44b319ed7bcac9
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2021-06-24 02:33:53 -0500

    de float a string

--- controllers/preliquidacion.go
+++ controllers/preliquidacion.go
@@ -525,7 +525,7 @@ func CargarDatosRetefuente(cedula int) (reglas string) {
 				reglas = reglas + "pensionado(no)."
 			}
 
-			reglas = reglas + "intereses_vivienda(" + tempPersonaNatural.InformacionPersonaNatural.InteresViviendaAfc + ")."
+			reglas = reglas + "intereses_vivienda(" + fmt.Sprintf("%f", tempPersonaNatural.InformacionPersonaNatural.InteresViviendaAfc) + ")."
 
 		} else {
 			fmt.Println("No existen datos sobre esa persona")
