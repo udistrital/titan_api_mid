81a50a24b3499664368904fd35e27cbc98a2152c
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2021-08-11 14:27:41 -0500

    ajuste rete

--- golog/utilidades.go
+++ golog/utilidades.go
@@ -387,12 +387,10 @@ func CalcularReteFuenteSal(tipoPreliquidacionString, reglas string, listaDescuen
 	}
 
 	saldo=ingresos-deduccion_salud
-	if saldo < 0
-	{
+	if saldo < 0	{
 		temp_reglas = temp_reglas + "ingresos(" + strconv.Itoa(ingresos) + ")."
 		deduccion_salud=0
-	}else
-	{
+	}else	{
 		temp_reglas = temp_reglas + "ingresos(" + strconv.Itoa(ingresos-deduccion_salud) + ")."
 	}
 
