163ab252cb39597916d5e792d6b7483822be12be
jorgec815 <jorgecortes815@gmail.com>
2022-03-21 18:19:59 -0500

    Fix: ajuste de cumplido General

--- controllers/contrato_preliquidacion.go
+++ controllers/contrato_preliquidacion.go
@@ -32,6 +32,8 @@ func (c *CumplidoController) ActualizarCumplido() {
 	var aux map[string]interface{}
 	var contrato []models.Contrato
 	var contrato_preliquidacion []models.ContratoPreliquidacion
+	var id int
+	cumplidoCompleto := true
 	ano := c.Ctx.Input.Param(":ano")
 	mes := c.Ctx.Input.Param(":mes")
 	numeroContrato := c.Ctx.Input.Param(":contrato")
@@ -49,8 +51,47 @@ func (c *CumplidoController) ActualizarCumplido() {
 			if err := request.SendJson(beego.AppConfig.String("UrlTitanCrud")+"/contrato_preliquidacion/"+strconv.Itoa(contrato_preliquidacion[0].Id), "PUT", &aux, contrato_preliquidacion[0]); err == nil {
 				fmt.Println("Cumplido actualizado")
 				c.Data["json"] = map[string]interface{}{"Success": true, "Status": "200", "Message": "Successful", "Data": contrato_preliquidacion}
-				c.ServeJSON()
+				query := "ContratoId.Documento:" + contrato[0].Documento + ",PreliquidacionId.Mes:" + mes + ",PreliquidacionId.Ano:" + ano
+				if err := request.GetJson(beego.AppConfig.String("UrlTitanCrud")+"/contrato_preliquidacion?limit=-1&query="+query, &aux); err == nil {
+					LimpiezaRespuestaRefactor(aux, &contrato_preliquidacion)
+					if contrato_preliquidacion[0].Id != 0 {
+						for i := 0; i < len(contrato_preliquidacion); i++ {
+							if contrato_preliquidacion[i].ContratoId.NumeroContrato != "GENERAL"+mes {
+								if !contrato_preliquidacion[i].Cumplido {
+									cumplidoCompleto = false
+								}
+							} else {
+								id = i
+							}
+						}
+						if cumplidoCompleto {
+							//Actualizar el cumplido del contrato General
+							fmt.Println("Actualizando cumplido general")
+							contrato_preliquidacion[id].Cumplido = true
+							if err := request.SendJson(beego.AppConfig.String("UrlTitanCrud")+"/contrato_preliquidacion/"+strconv.Itoa(contrato_preliquidacion[id].Id), "PUT", &aux, contrato_preliquidacion[id]); err == nil {
+								fmt.Println("contrato general actualizado")
+							} else {
+								fmt.Println("Error al actualizar cumplido general: ", err)
+								c.Data["message"] = "Error al actualizar cumplido general: " + err.Error()
+								c.Abort("404")
+							}
+						}
+					} else {
+						fmt.Println("Error al obetener cumplidos: ", err)
+						c.Data["message"] = "Error al obtener cumplidos: " + err.Error()
+						c.Abort("404")
+					}
+				} else {
+					fmt.Println("Error al actualizar cumplido: ", err)
+					c.Data["message"] = "Error al actualizar cumplido: " + err.Error()
+					c.Abort("404")
+				}
+			} else {
+				fmt.Println("Error al actualizar cumplido: ", err)
+				c.Data["message"] = "Error al actualizar cumplido: " + err.Error()
+				c.Abort("404")
 			}
+
 		} else {
 			fmt.Println("Error al obtener contrato preliquidación")
 			c.Data["message"] = "Error al obtener contrato preliquidacion " + err.Error()
