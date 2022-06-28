ffb19315e4dbe9bed53760602bfade7437053fe8
jorgec815 <jorgecortes815@gmail.com>
2022-06-28 16:29:49 -0500

    Feat: Fecha de novedad dinámica

--- models/categoria_beneficiario.go
+++
@@ -1,8 +0,0 @@
-package models
-
-
-type CategoriaBeneficiario struct {
-	Id                          int    `orm:"column(id);pk"`
-	NombreTipoBeneficiario      string `orm:"column(nombre_tipo_beneficiario)"`
-	DescripcionTipoBeneficiario string `orm:"column(descripcion_tipo_beneficiario)"`
-}
