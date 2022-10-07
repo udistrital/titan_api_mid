bff899bde11be796b9edd4a8e64153dd8735ee0b
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2022-10-07 18:12:10 -0500

    ajuste contrato id

--- controllers/detalle_preliquidacion.go
+++ controllers/detalle_preliquidacion.go
@@ -306,7 +306,7 @@ func EliminarValorNovedad(novedad models.Novedad, fecha_actual time.Time) (mensa
 	mesFin := int(novedad.FechaFin.Month())
 
 	for i := int(fecha_actual.Month()); i <= mesFin; i++ {
-		var query = "ContratoId.Id:" + strconv.Itoa(novedad.ContratoId) + ",PreliquidacionId.Ano:" + strconv.Itoa(fecha_actual.Year()) + ",PreliquidacionId.Mes:" + strconv.Itoa(i)
+		var query = "ContratoId.Id:" + strconv.Itoa(novedad.ContratoId.Id) + ",PreliquidacionId.Ano:" + strconv.Itoa(fecha_actual.Year()) + ",PreliquidacionId.Mes:" + strconv.Itoa(i)
 		fmt.Println("consulta contrato preliquidacion ", query)
 		if err := request.GetJson(beego.AppConfig.String("UrlTitanCrud")+"/contrato_preliquidacion?limit=-1&query="+query, &aux); err == nil { //obtiene el contrato_preliquidacion de ese mes y año
 			LimpiezaRespuestaRefactor(aux, &contratoPreliquidacion)
